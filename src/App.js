ad7f01ededdfdefb2edd72dcf597aae88d3544ab
Indra Pratama Putra <indra.symfonia@gmail.com>
2017-07-03 16:47:21 +0700

    updates

--- src/App.js
+++ src/App.js
@@ -3,32 +3,10 @@ import {h, Component} from 'preact'
 import fetch from 'unfetch'
 import _ from 'lodash'
 
+import ImageCarousel from './Components/ImageCarousel'
+import TabComponent from './Components/Tabs'
+
 const DATA = "http://localhost:3000/product.json";
-
-const TabData = ({DATA}) => {
-  fetch(DATA)
-    .then(resp => resp.json())
-    .then(tabData => {
-      console.log(tabData);
-    })
-}
-
-function getData(link) {
-  var data;
-  fetch(link)
-    .then(resp => resp.json())
-    .then(respData => {
-      var tabData = _.clone(respData);
-      var objectKey = _.keys(tabData);
-      console.log(objectKey);
-    })
-
-      // data.push(Description, Review)
-
-    return data;
-}
-
-
 
 const tabs = [{
   name: 'Tab 1',
@@ -42,19 +20,35 @@ const tabs = [{
   content: 'Content for 3'
 }];
 
+const getData = (dataSrc) => {
+  var arrayData = null;
+  fetch(dataSrc)
+    .then(resp => resp.json())
+    .then(respData => {
+      var tabDatas = _.clone(respData);
+      var tabArray = _.map(tabDatas, function(value,index) {
+        return {
+          name: tabDatas.index,
+          content: tabDatas.value,
+        }
+      })
+      arrayData = new Array(tabArray);
+    })
+    .catch(err => console.error(err));
 
-
-
-import ImageCarousel from './Components/ImageCarousel'
-import TabComponent from './Components/Tabs'
-
+  var result = arrayData;
+  return result;
+}
 
 export default class App extends Component {
+
+
+
   render() {
+    const test = getData(DATA);
     return <div className="App">
       <TabComponent tabs={tabs} firstSelect={0} />
-      {/* <ImageCarousel Data={DATA} /> */}
-      {console.log(getData(DATA))}
+      {console.log(test,tabs)}
     </div>
   }
 }
